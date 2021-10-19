7d32769d6ac74470dc2e0297699545a4ca463b2d
okatsuo <rafael.goncanna@gmail.com>
2021-10-19 07:20:11 -0300

    feat: jwt token to user authentication

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { sign } from 'jsonwebtoken';
+import prismaClient from '../prisma';
 
 interface IAccessTokenReponse {
   access_token: string
@@ -31,7 +33,35 @@ class AuthenticateUserService {
       }
     })
 
-    return response.data
+    const { login, id, avatar_url, name } = response.data
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    })
+    if (!user) {
+      await prismaClient.user.create({
+        data: {
+          github_id: id,
+          avatar_url,
+          login,
+          name
+        }
+      })
+    }
+
+    const token = sign({
+      user: {
+        name: user.name,
+        avatar_url: user.avatar_url,
+        id: user.id
+      }
+    },
+      process.env.JWT_SECRET,
+      { subject: user.id, expiresIn: '1d' }
+    )
+
+    return { token, user }
   }
 }
 
