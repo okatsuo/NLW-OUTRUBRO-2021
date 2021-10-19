9b3d0f1b132cce4eddbe4f5c94a67cbdcaae7c95
okatsuo <rafael.goncanna@gmail.com>
2021-10-19 07:05:09 -0300

    feat: controller to github authentication and prisma user migration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
-import express from 'express'
+import 'dotenv/config'
+import express, { response } from 'express'
+import { router } from './routers'
 
 const app = express()
+app.use(express.json())
+
+app.use(router)
+
+app.get('/github', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query
+  res.json(code)
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+interface IAccessTokenReponse {
+  access_token: string
+}
+
+interface IUserReponse {
+  avatar_url: string,
+  login: string,
+  id: number,
+  name: string
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenReponse>(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+      },
+      headers: {
+        "Accept": "application/json"
+      }
+    })
+
+    const response = await axios.get<IUserReponse>("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
+    })
+
+    return response.data
+  }
+}
+
+export { AuthenticateUserService }
