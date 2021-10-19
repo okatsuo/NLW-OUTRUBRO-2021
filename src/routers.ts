90fca18f5cc059cecbe0003e3e920e78a19a6143
okatsuo <rafael.goncanna@gmail.com>
2021-10-19 08:59:12 -0300

    feat: ensure to be authenticated user to access user profile

--- src/routers.ts
+++ src/routers.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+import { UserProfileController } from './controllers/UserProfileController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router()
@@ -11,5 +12,7 @@ router.post("/authenticate", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 
 router.get("/messages/last3", new GetLast3MessagesController().handle)
+
+router.get('/user-profile', ensureAuthenticated, new UserProfileController().handle)
 
 export { router }
