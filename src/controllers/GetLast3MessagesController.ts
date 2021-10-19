56e515d957c9db93f972b22b3f8fe359483c2e56
okatsuo <rafael.goncanna@gmail.com>
2021-10-19 08:29:35 -0300

    feat: ensure getLast3Messages return the data

---
+++ src/controllers/GetLast3MessagesController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { io } from '../app';
+import { CreateMessageService } from '../services/CreateMessageService';
+import { GetLast3MessagesService } from '../services/GetLast3MessagesService';
+
+class GetLast3MessagesController {
+  async handle(req: Request, res: Response) {
+    try {
+      const service = new GetLast3MessagesService();
+
+      const result = service.execute()
+      return res.json(result)
+    } catch (error) {
+      res.json({ errorCode: error.message })
+    }
+  }
+}
+
+export { GetLast3MessagesController }
